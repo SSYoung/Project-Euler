0442a6a8a165c94cba3286c5fa7b81f409f9a622
SSYoung <stefan.gc.young@gmail.com>
2016-02-21 17:08:36 -0500

    Sieve of Erastosthenes

--- PrimeSieve.java
+++ PrimeSieve.java
@@ -8,10 +8,13 @@ public class PrimeSieve {
     }
     private boolean[] fillSieve(int n) {
         boolean[] p = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            p[i] = true;
+        }
         for (int i = 2; i*i <= n; i++) {
             if (p[i]) {
                 for (int j = i; i*j <= n; j++) {
-                    isPrime[i*j] = false;
+                    p[i*j] = false;
                 }
             }
         }
